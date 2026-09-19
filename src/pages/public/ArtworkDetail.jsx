8d30e4528e2e50b0bb99f057f2f7f11668682a1d
fengyubo1212-ux <fengyubo1212@gmail.com>
2026-09-20 00:56:43 +0800

    修复：统一作品描述文章的段落格式

--- src/pages/public/ArtworkDetail.jsx
+++ src/pages/public/ArtworkDetail.jsx
@@ -59,7 +59,11 @@ export default function ArtworkDetail() {
             </span>
           )}
         </div>
-        <p className="body" ref={bodyRef}>{data.description}</p>
+        <div className="body" ref={bodyRef}>
+          {data.description.split(/\n\n+/).map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
+          ))}
+        </div>
       </div>
       {lightbox && (
         <Lightbox src={data.image_url} alt={data.title} onClose={() => setLightbox(false)} />
